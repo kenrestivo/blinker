b5e03e158a351b17c434f616f95b5ca6016ad73b
ken restivo <ken@restivo.org>
2014-02-28 19:24:55 -0800

    Begin adding files.

---
+++ README.md
@@ -0,0 +1,14 @@
+# blinker
+
+Blinker
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2014 ken restivo <ken@restivo.org>
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,35 @@
+(defproject blinker "0.1.0-SNAPSHOT"
+  :description "Web/mobile app for binary counting practice"
+  :url "http://restivo.org/blinker"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [weasel "0.1.0"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [ankha "0.1.1"]
+                 [om "0.5.0"]]
+
+  
+  :source-paths ["src"]
+
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.1"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+  
+  :cljsbuild {:builds [{:id "dev" 
+                        :source-paths ["src"]
+                        :compiler {:output-to "blinker.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map  true}}
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "blinker-min.js"
+                                   ;; :output-wrapper true ;; don't know why this would be necessary?
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :source-map  "blinker.js.map"
+                                   :preamble ["react/react.min.js"]
+                                   :externs ["react/externs/react.js"]}}]})
