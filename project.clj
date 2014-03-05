ae312da9d36ba8f561b7cdf1f1cdedc7ab2649e9
ken restivo <ken@restivo.org>
2014-03-05 09:04:11 -0800

    Upgrade to latest om and clojurescript.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://restivo.org/blinker"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [weasel "0.1.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.cemerick/piggieback "0.1.3"]
                  [ankha "0.1.1"]
-                 [om "0.5.0"]]
+                 [om "0.5.1"]]
 
 
   :source-paths ["clj-src"]
